da3c948aa3edf46e2b73f79e91c496cd35c947cf
Noah Lemen <noah.lemen@gmail.com>
2014-03-10 14:36:06 -0400

    lights, camera tilt

---
+++ Assets/TiltCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TiltCamera : MonoBehaviour {
+
+	public float maxoffset = 2f;
+	
+	float zoffset = 0f;
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey(KeyCode.A)){
+			zoffset = Mathf.Lerp(zoffset, maxoffset, Time.deltaTime);
+		}else if (Input.GetKey(KeyCode.D)){
+			zoffset = Mathf.Lerp(zoffset, -maxoffset, Time.deltaTime);
+		}else {
+			zoffset = Mathf.Lerp(zoffset, 0f, Time.deltaTime);
+		}
+
+		transform.eulerAngles = new Vector3(9.418701f,0f,zoffset);
+
+	}
+}
